3377aeb25d5efe3ccf6e01f192e31e3094e50fc3
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-07-01 17:26:37 -0500

    Socket.io: store.subscribe and 'connection' emmit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 import server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new server().attach(8090);
+
+  store.subscribe( () => {
+    return io.emmit('state', store.getState().toJS() );
+  });
+
+  io.on('connection', (socket) => {
+    socket.emmit('state', store.getState().toJS() );
+  });
+
 }
